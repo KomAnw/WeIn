c265c43c9d72f30c87c4b635ee0c71441db9657a
gkomissarov <komissarov2525@gmail.com>
2023-03-14 01:38:01 +0300

    image fix

--- src/sections/Map.tsx
+++ src/sections/Map.tsx
@@ -1,12 +1,19 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import styled from 'styled-components';
 import { Anchor } from 'src/components/Anchor';
-import 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/leaflet.css';
 
 export const Map = () => {
   useEffect(() => {
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+      iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+    });
     const map = L.map('map').setView([59.93395, 30.29403], 17.5);
 
     if (map) {
@@ -35,7 +42,4 @@ const MapContainer = styled.div.attrs({ id: 'map' })`
   height: 500px;
   margin-top: 30px;
   z-index: 10;
-  .leaflet-default-icon-path {
-    background-image: url(https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png);
-  }
 `;
