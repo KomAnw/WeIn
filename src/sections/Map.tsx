061eaa4d31506aa5522edb92f4568cf6ce086b72
gkomissarov <komissarov2525@gmail.com>
2023-03-14 01:47:56 +0300

    hope now fixed

--- src/sections/Map.tsx
+++ src/sections/Map.tsx
@@ -1,18 +1,19 @@
-/* eslint-disable global-require */
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import styled from 'styled-components';
 import { Anchor } from 'src/components/Anchor';
 import 'leaflet/dist/leaflet.css';
+import iconTwo from 'assets/marker-icon-2x.png';
+import icon from 'assets/marker-icon.png';
+import shadow from 'assets/marker-shadow.png';
 
 export const Map = () => {
   useEffect(() => {
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-      iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-      shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+      iconRetinaUrl: iconTwo,
+      iconUrl: icon,
+      shadowUrl: shadow,
     });
     const map = L.map('map').setView([59.93395, 30.29403], 17.5);
 
